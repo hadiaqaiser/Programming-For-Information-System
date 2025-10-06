96c76adf6badd31bbd4dde9ab9ac0c00540c7ade
hadiaqaiser <20069602@mydbs.ie>
2025-10-06 09:49:30 +0100

    ex4.js ask for input

---
+++ ex4.js
@@ -0,0 +1,6 @@
+// Reads: "factors : multiples"
+// //  Shows: "sum : factors : multiples"
+
+function processFactorsMultiples() {
+  // Ask user for input
+  let input = prompt("Enter: factors : multiples\nEx: 3 5 : 10 15 20");
