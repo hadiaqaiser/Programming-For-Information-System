b610446a3ff8a71449c3482714f33ad5f8849da2
hadiaqaiser <20069602@mydbs.ie>
2025-10-06 09:50:04 +0100

    ex4.js check if input valid

--- ex4.js
+++ ex4.js
@@ -4,3 +4,10 @@
 function processFactorsMultiples() {
   // Ask user for input
   let input = prompt("Enter: factors : multiples\nEx: 3 5 : 10 15 20");
+
+    // Check if valid
+  if (!input || !input.includes(":")) {
+    alert("Wrong format. Use: factors : multiples");
+    return;
+  }
+
